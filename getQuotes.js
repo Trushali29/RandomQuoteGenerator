f2072e72beeda2c26c406702cf57aaa5188e3cca
Trushali29 <84562990+Trushali29@users.noreply.github.com>
2023-06-02 12:30:43 +0530

    Add files via upload

---
+++ getQuotes.js
@@ -0,0 +1,7 @@
+
+const data= [{"quote":"Life isn’t about getting and having, it’s about giving and being.","author":"Kevin Kruse"},
+{"quote":"Whatever the mind of man can conceive and believe, it can achieve.","author":"Napoleon Hill"},
+{"quote":"Strive not to be a success, but rather to be of value.","author":"Albert Einstein"},
+{"quote":"Two roads diverged in a wood, and I—I took the one…raveled by, And that has made all the difference.","author":"Robert Frost"},
+{"quote":"You miss 100% of the shots you don’t take.","author":"Wayne Gretzky"}
+]
